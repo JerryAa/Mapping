32cfab2ce87b5ba686bc4430e60e8ee74ee30a0f
JerryAa <jerrya15@yahoo.com>
2019-03-16 14:36:01 -0500

    Added dijkstra function and fixed prnt()

--- shortest_dij.py
+++ shortest_dij.py
@@ -8,35 +8,49 @@ import collections
 class Graph(object): 
     def __init__(self, graph, start='a'): 
         self.graph = graph 
-        unvisited = ' '.join(string.ascii_letters[0:6]).split()
-        visited = collections.deque()  
-        prev = str() 
-        shortest_distance_from_start = [math.inf] * len(graph[0]) 
-        print(shortest_distance_from_start) 
+        self.unvisited = ' '.join(string.ascii_letters[0:6]).split()
+        self.visited = collections.deque()  
+        self.prev = str() 
+        self.shortest_distance_from_start = [math.inf] * len(graph[0]) 
+        self.neighbours = dict() 
 
 
     def prnt (self): 
         ''' 
         print adjacency matrix 
         '''
-        for r in range(0, len(self.graph)): 
-            for c in range(0, len(self.graph[0])): 
-                print(f'{self.graph[r][c]} ', end=" ")
+        for r in self.graph: 
+            for c in r: 
+                print(f'{c}', end=" ") 
             print("\n") 
 
         return True 
             
+    def dijkstra(self): 
+        vrtices = ' '.join(string.ascii_letters[0:6]).split()
+        for r in enumerate(vrtices): # note r[1] s a tuple 
+            for c in enumerate(vrtices): # note c[1] s a tuple 
+                if (self.graph[r[0]][c[0]] == 0): 
+                    continue 
+                if r[1] in self.neighbours.keys(): 
+                    self.neighbours[r[1]].append(c[1]) 
+                else :
+                    self.neighbours[r[1]] = list() 
+                    self.neighbours[r[1]].append(c[1]) 
+
+        print(self.neighbours)
  
 def main(): 
     
 
     graph = [
-            [0, 7, 9, 0, 0, 14], 
-            [7, 0, 10, 15, 0, 0], 
-            [9, 10, 0, 0, 0, 2], 
-            [0, 15, 0, 0, 6, 0],
-            [0, 0, 0, 6, 0, 9],
-            [14, 0, 2, 0, 9, 0]]
+          [0, 7, 9, 0, 0, 14], 
+          [7, 0, 10, 15, 0, 0],
+          [9, 10, 0, 11, 0, 2],
+          [0, 15, 11, 0, 6, 0],
+          [0, 0, 0, 6, 0, 9],
+          [14, 0, 2, 0, 9, 0]] 
+
 
     g = Graph(graph) 
     print(g.prnt()) 
